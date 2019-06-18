28ab5ad496a89e4a33130965ff3f0d83d07e6488
friedbergjon <friedberg_jon@hotmail.com>
2019-06-18 10:07:28 -0400

    add_item and add_event front end components

---
+++ client/src/components/AddEvent.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Redirect} from "react-router-dom"
+
+class CreateEvent extends Component {
+    constructor(){
+        super();
+            this.state = {
+                name: "",
+                date: "",
+                redirect: false
+            }
+            this.handleChange=this.handleChange.bind(this)
+            this.handleSubmit=this.handleSubmit.bind(this)
+        }
+            handleChange(event){
+                const name = event.target.name;
+                const value = event.target.value;
+
+                this.setState({
+                    [name]: value
+                })
+            }
+            async handleSubmit(event){
+                event.preventDefault();
+
+                await axios.post("http://localhost:4567/event", {
+                    name: this.state.name,
+                    date:this.state.date,
+                  
+                })
+                this.setState({
+                    redirect:true
+                })
+            }
+            render(){
+                return (
+                    <div>
+                    {this.state.redirect ? <Redirect to="/wishlist"/>:null}
+                    <form
+                    onChange={this.handleChange}
+                    onSubmit={this.handleSubmit}
+                    >
+                        <input
+                        name="name"
+                        type="text"
+                        placeholder="NAME OF EVENT"
+                        value={this.state.name}
+                        />
+                         <input
+                        name="date"
+                        type="date"
+                        placeholder="DATE"
+                        value={this.state.date}
+                        />
+                        <input type="submit"
+                        />
+                        </form></div>
+                )
+            }
+    }
+
+    export default CreateEvent
---
+++ client/src/components/CreateItem.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Redirect} from "react-router-dom"
+
+class CreateItem extends Component {
+    constructor(){
+        super();
+            this.state = {
+                name: "",
+                price: "",
+                link: "",
+                redirect: false
+            }
+            this.handleChange=this.handleChange.bind(this)
+            this.handleSubmit=this.handleSubmit.bind(this)
+        }
+            handleChange(event){
+                const name = event.target.name;
+                const value = event.target.value;
+
+                this.setState({
+                    [name]: value
+                })
+            }
+            async handleSubmit(event){
+                event.preventDefault();
+
+                await axios.post("http://localhost:4567/item", {
+                    name: this.state.name,
+                    price:this.state.price,
+                    link:this.state.link
+                })
+                this.setState({
+                    redirect:true
+                })
+            }
+            render(){
+                return (
+                    <div>
+                    {this.state.redirect ? <Redirect to="/wishlist"/>:null}
+                    <form
+                    onChange={this.handleChange}
+                    onSubmit={this.handleSubmit}
+                    >
+                        <input
+                        name="name"
+                        type="text"
+                        placeholder="NAME"
+                        value={this.state.name}
+                        />
+                         <input
+                        name="price"
+                        type="number"
+                        placeholder="PRICE"
+                        value={this.state.price}
+                        />
+                         <input
+                        name="link"
+                        type="text"
+                        placeholder="LINK"
+                        value={this.state.link}
+                        />
+                        <input type="submit"
+                        />
+                        </form></div>
+                )
+            }
+    }
+
+    export default CreateItem
