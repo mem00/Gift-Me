8eeb6fef7c0060ac7fa6376cf3251032a9f27bcd
friedbergjon <friedberg_jon@hotmail.com>
2019-06-18 12:26:21 -0400

    App.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+import axios from 'axios';
+import AddEvent from './components/AddEvent';
+import AddItem from './components/AddItem';
+import CreateWishlist from './components/CreateWishlist';
+import CreateWishlistForm from './components/CreateWishlistForm';
+import Search from './components/SearchForm';
 import SearchForm from './components/SearchForm';
+import ShowWishlist from './components/ShowStudent';
 
 function App() {
   return (
     <div className="App">
-    <SearchForm/>
+       <Switch>
+          <Route exact path="/wishlist/:id/event/create" component={AddEvent} />
+          <Route exact path="/wishlist/:id/item/create" component={AddItem} />
+          <Route exact path="/" component={CreateWishlist} />
+          <Route exact path="//:id/edit" component={CreateWishlistForm} />
+          <Route exact path="/???" component={Search} />
+          <Route exact path="/???" component={SearchForm} />
+          <Route exact path="/wishlist" component={ShowWishlist} />
+        </Switch>
+  
     </div>
   );
 }
