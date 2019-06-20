97ada556948c504b517b183465388f9530c1e9e0
Michael McGuire <michael3mcguire@gmail.com>
2019-06-20 16:59:16 -0400

    initial heroku commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors')
+const path = require('path');
 const {wishlistRouter} = require('./routes/wishlist')
 const {eventRouter} = require('./routes/event')
 const {itemRouter} = require('./routes/item')
@@ -9,6 +10,9 @@ const {personRouter} = require('./routes/person')
 
 const PORT = process.env.PORT || 4567;
 const app = express();
+
+// Static hosting for built files
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use(cors())
 app.use(logger('dev'))
@@ -31,6 +35,13 @@ app.use('/event', eventRouter)
 app.use('/item', itemRouter)
 
 app.use('/person', personRouter)
+
+
+// In production, any request that doesn't match a previous route
+// should send the front-end application, which will handle the route.
+if (process.env.NODE_ENV == "production") {
+    app.use('*', (req, res) => res.sendFile(path.join(__dirname, './client/build', "index.html")));
+    }
 
 
 app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
